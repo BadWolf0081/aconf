ef07c8ab30e9d187a2e0ccc7e8b631739397394e
Robbie <57416363+BadWolf0081@users.noreply.github.com>
2025-04-26 11:23:39 -0300

    test

--- scripts/aegis.sh
+++ scripts/aegis.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# version 2.2.7
+# version 2.2.8
 
 #Version checks
 Ver42aegis="1.6"
@@ -644,7 +644,7 @@ if [[ $(grep useSender $aconf_versions | awk -F "=" '{ print $NF }' | awk '{ gsu
 fi
 
 # check aegis running
-aegis_check=$(ps -ef | grep com.pokemod.aegis:mapping | awk '{print $9}')
+aegis_check=$(ps -e | grep com.pokemod.aegis:mapping | awk '{print $9}')
 if [[ -z $aegis_check ]] && [[ -f /data/local/tmp/aegis_config.json ]] ;then
   logger "aegis not running at execution of aegis.sh, starting it"
   if [ $android_version -ge 9 ]; then
